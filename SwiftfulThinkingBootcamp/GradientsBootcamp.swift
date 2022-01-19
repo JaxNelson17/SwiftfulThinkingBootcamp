6c3dfd32e9fb96f35bdd9384d74228d78abb1ac0
JaxNelson17 <jaxnel17@gmail.com>
2022-01-19 15:44:14 -0700

    finished video number 4 and 5

---
+++ SwiftfulThinkingBootcamp/GradientsBootcamp.swift
@@ -0,0 +1,38 @@
+//
+//  GradientsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 1/19/22.
+//
+
+import SwiftUI
+
+struct GradientsBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(
+                //Color.red
+//                LinearGradient(
+//                    gradient: Gradient(colors: [Color.red, Color.blue]),
+//                    startPoint: .topLeading,
+//                    endPoint: .bottom)
+//                RadialGradient(
+//                    gradient: Gradient(colors: [Color.red, Color.blue]),
+//                    center: .topLeading,
+//                    startRadius: 5,
+//                    endRadius: 400)
+                AngularGradient(
+                    gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha:1))]),
+                    center: .topLeading,
+                    angle: .degrees(180 + 45))
+            )
+        // [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha:1))]
+            .frame(width: 300, height: 200)
+    }
+}
+
+struct GradientsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientsBootcamp()
+    }
+}
