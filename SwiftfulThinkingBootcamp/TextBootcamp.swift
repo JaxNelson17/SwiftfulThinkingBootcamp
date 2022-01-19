eacc69468c1ed5f6d02b7793a1baa8b7f2ab9ade
JaxNelson17 <jaxnel17@gmail.com>
2022-01-19 14:13:39 -0700

    Finished Video number 2

---
+++ SwiftfulThinkingBootcamp/TextBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  TextBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 1/19/22.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            //.font(.body)
+            //.fontWeight(.semibold)
+            //.bold()
+            //.underline()
+//            .underline(true
+//                       , color: Color.red)
+//            .italic()
+//            .strikethrough(true, color: Color.green)
+//            .font(.system(size: 24, weight: .semibold, design: .serif))
+//            .baselineOffset(-50.0)
+//            .kerning(10)
+            .multilineTextAlignment(.leading)
+            .foregroundColor(.red)
+            .frame(width: 200, height: 100, alignment: .leading)
+                .minimumScaleFactor(0.1)
+    }
+}
+
+struct TextBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootcamp()
+    }
+}
