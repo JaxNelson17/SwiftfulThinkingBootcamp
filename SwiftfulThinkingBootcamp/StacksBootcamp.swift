fdebb7f24d92607d6bd2a062c9e8f06bb4f7d66f
JaxNelson17 <jaxnel17@gmail.com>
2022-01-20 14:34:44 -0700

    Finished video number 10

---
+++ SwiftfulThinkingBootcamp/StacksBootcamp.swift
@@ -0,0 +1,89 @@
+//
+//  StacksBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 1/20/22.
+//
+
+import SwiftUI
+
+struct StacksBootcamp: View {
+    // Vstacks -> Vertical
+    // Hstacks -> Horizontal
+    // Zstacks -> Index (back to front)
+    var body: some View {
+        VStack(spacing: 50) {
+            
+            ZStack {
+                Circle()
+                    .frame(width: 100, height: 100)
+                
+                Text("1")
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+            
+            Text("1")
+                .font(.title)
+                .foregroundColor(.white)
+                .background(
+                    Circle()
+                        .frame(width: 100, height: 100)
+                )
+        }
+    }
+}
+
+struct StacksBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StacksBootcamp()
+    }
+}
+
+/*
+ ZStack(alignment: .trailing, content: {
+     Rectangle()
+         .fill(Color.red)
+         .frame(width: 200, height: 200)
+
+     Rectangle()
+         .fill(Color.green)
+         .frame(width: 150, height: 150)
+
+     Rectangle()
+         .fill(Color.orange)
+         .frame(width: 100, height: 100)
+ })
+ 
+ ZStack(alignment: .top) {
+     Rectangle()
+         .fill(Color.yellow)
+         .frame(width: 350, height: 500, alignment: .center)
+     
+     VStack(alignment: .leading, spacing: 30) {
+         Rectangle()
+             .fill(Color.red)
+             .frame(width: 150, height: 150)
+         
+         Rectangle()
+             .fill(Color.green)
+             .frame(width: 100, height: 100)
+         
+         HStack(alignment: .bottom) {
+             Rectangle()
+                 .fill(Color.purple)
+                 .frame(width: 50, height: 50)
+             
+             Rectangle()
+                 .fill(Color.pink)
+                 .frame(width: 75, height: 75)
+             
+             Rectangle()
+                 .fill(Color.blue)
+                 .frame(width: 25, height: 25)
+         }
+         .background(Color.white)
+     }
+     .background(Color.black)
+ }
+ */
