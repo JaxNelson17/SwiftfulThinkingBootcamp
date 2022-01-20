7ff4f6f93728b78f102cf8b16d79c4d011f49a9e
JaxNelson17 <jaxnel17@gmail.com>
2022-01-20 14:52:54 -0700

    Got the file for video 12

---
+++ SwiftfulThinkingBootcamp/SpacingBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  SpacingBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 1/20/22.
+//
+
+import SwiftUI
+
+struct SpacingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpacingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SpacingBootcamp()
+    }
+}
