5511d9b727eb678f138c026b1daf8758ad18df53
JaxNelson17 <jaxnel17@gmail.com>
2022-01-24 16:14:15 -0700

    Got up to video 13 done

--- SwiftfulThinkingBootcamp/SpacingBootcamp.swift
+++ SwiftfulThinkingBootcamp/SpacingBootcamp.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct SpacingBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(spacing: 0) {
+                Image(systemName: "xmark")
+                Spacer()
+                Image(systemName: "gear")
+            }
+            .font(.title)
+            .padding(.horizontal)
+            Spacer()
+            Rectangle()
+                .frame(height: 55)
+        }
     }
 }
 
@@ -18,3 +29,36 @@ struct SpacingBootcamp_Previews: PreviewProvider {
         SpacingBootcamp()
     }
 }
+
+//HStack(spacing: 0) {
+//
+//    Spacer(minLength: 0)
+//        .frame(height: 10)
+//        .background(Color.orange)
+//
+//    Rectangle()
+//        .frame(width: 50, height: 50)
+//
+//    Spacer()
+//        .frame(height: 10)
+//        .background(Color.orange)
+//
+//    Rectangle()
+//        .fill(Color.red)
+//        .frame(width: 50, height: 50)
+//
+//    Spacer()
+//        .frame(height: 10)
+//        .background(Color.orange)
+//
+//    Rectangle()
+//        .fill(Color.green)
+//        .frame(width: 50, height: 50)
+//
+//    Spacer(minLength: 0)
+//        .frame(height: 10)
+//        .background(Color.orange)
+//}
+//.background(Color.yellow)
+//.padding(.horizontal, 200)
+//.background(Color.blue)
