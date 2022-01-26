1e56a98015ebc72da06dfef14c5a7c4eff721317
JaxNelson17 <jaxnel17@gmail.com>
2022-01-26 13:48:35 -0700

    finished video number 22

---
+++ SwiftfulThinkingBootcamp/BindingBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  BindingBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 1/26/22.
+//
+
+import SwiftUI
+
+struct BindingBootcamp: View {
+    
+    @State var backgroundColor: Color = Color.green
+    @State var title: String = "Title"
+    
+    var body: some View {
+        ZStack {
+            backgroundColor.ignoresSafeArea()
+            
+            VStack {
+                
+                Text(title)
+                    .foregroundColor(.white)
+                ButtonView(backgroundColor: $backgroundColor, title: $title)
+            }
+        }
+    }
+}
+
+struct ButtonView: View {
+    
+    @Binding var backgroundColor: Color
+    @Binding var title: String
+    @State var buttonColor: Color = Color.blue
+    
+    var body: some View {
+        Button(action: {
+            backgroundColor = Color.orange
+            buttonColor = Color.pink
+            title = "New Title"
+        }, label: {
+            Text("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(buttonColor)
+                .cornerRadius(10)
+        })
+    }
+}
+
+struct BindingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingBootcamp()
+    }
+}
