4257e2ff88a892af7f8372c9d812a8ad0cc5524f
JaxNelson17 <jaxnel17@gmail.com>
2022-02-07 16:34:45 -0700

    finished video 56

---
+++ SwiftfulThinkingBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 2/7/22.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
