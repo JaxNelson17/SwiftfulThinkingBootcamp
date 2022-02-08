10c15b63119e1ce62caec575103e45cc67c4543a
JaxNelson17 <jaxnel17@gmail.com>
2022-02-08 14:20:29 -0700

    fininshed video number 57

---
+++ SwiftfulThinkingBootcamp/ButtonStylesBootcamp.swift
@@ -0,0 +1,69 @@
+//
+//  ButtonStylesBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jaxson Nelson on 2/8/22.
+//
+
+import SwiftUI
+
+struct ButtonStylesBootcamp: View {
+    var body: some View {
+        VStack {
+            
+            Button {
+                
+            } label: {
+                Text("Button Style")
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 20))
+            .controlSize(.large)
+
+            
+            Button("Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.large)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.regular)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.small)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.mini)
+            .buttonStyle(.borderedProminent)
+        }
+        
+        .padding()
+    }
+}
+
+struct ButtonStylesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStylesBootcamp()
+            .preferredColorScheme(.dark)
+.previewInterfaceOrientation(.portrait)
+    }
+}
